b3e84965e85ceb170419b2fc6487608659fe454d
jasa <jasa.ksela@student.um.si>
2019-03-27 07:53:57 +0100

    dpdana podjetja

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,24 @@ public class Main {
 
         Artikli seznam1 = new Artikli();
         Artikel pivo = new Artikel("pivo", new BigDecimal("2"), new BigDecimal("1.2"), 3, "1234567891");
-        Artikel voda = new Artikel("pivo", new BigDecimal("1"), new BigDecimal("1.2"), 1, "1234567891");
+        Artikel voda = new Artikel("voda", new BigDecimal("1"), new BigDecimal("1.2"), 1, "1234567891");
         seznam1.getSeznamArtiklov().add(pivo);
         seznam1.getSeznamArtiklov().add(voda);
 
         Podjetje sava = new Podjetje("HENKEL MARIBOR d.o.o.", true,"58665765","6261752000","Industrijska ulica 23, Maribor, 2000 Maribor");
+
+	Podjetje Union = new Podjetje("Pivovarna Laško Union d.o.o.", true,"90355580","5049318000","Pivovarniška ulica 2, Ljubljana, 1000 Ljubljana");
+
+	Podjetje Union = new Podjetje("Akrapovič d.d.", true,"95023828","1387570000","Malo Hudo 8A, Malo Hudo, 1295 Ivančna Gorica");
+
+	
 
 
         Date trenutno = new Date();
         Racun skupaj = new Racun(seznam1,1,trenutno, "Janez novak", false,sava);
 
         boolean test = pivo.checkDigit("1291041500287");
+
 
         System.out.println(skupaj.toString());
 
